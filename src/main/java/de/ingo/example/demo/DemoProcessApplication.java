9c17c393c89d70030ad8a2956ed6a83a29e571a7
Ingo Richtsmeier <ingo.richtsmeier@camunda.com>
2026-02-23 15:22:50 +0100

    Add build config and files to deploy

--- src/main/java/de/ingo/example/demo/DemoProcessApplication.java
+++ src/main/java/de/ingo/example/demo/DemoProcessApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import io.camunda.client.annotation.Deployment;
 
 @SpringBootApplication
-@Deployment
+@Deployment(resources = { "ingo-process-app-demo-1.bpmn", "Start simple.form" })
 public class DemoProcessApplication {
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoProcessApplication.class, args);
 	}
