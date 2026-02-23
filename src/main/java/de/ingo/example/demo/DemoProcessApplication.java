ea0700e5857f6de204d20ada0ca096ecd3baa99e
Ingo Richtsmeier <ingo.richtsmeier@camunda.com>
2026-02-23 10:26:17 +0100

    initial camunda client app

---
+++ src/main/java/de/ingo/example/demo/DemoProcessApplication.java
@@ -0,0 +1,15 @@
+package de.ingo.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.camunda.client.annotation.Deployment;
+
+@SpringBootApplication
+@Deployment
+public class DemoProcessApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProcessApplication.class, args);
+	}
+}
